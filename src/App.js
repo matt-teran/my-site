05082c18fb319337c425c98c032925db1c7d6b90
Matt Teran <matthew.a.teran@gmail.com>
2020-08-23 02:27:23 -0500

    added jumbotron

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Aux from './hoc/Aux';
 import Backdrop from './components/UI/Backdrop/Backdrop';
 import Toolbar from './components/Navigation/Toolbar/Toolbar';
+import Jumbotron from './components/Pages/Art/Jumbotron/Jumbotron';
 
 function App() {
   return (
     <Aux>
-      <Backdrop/>
       <Toolbar/>
+      <Jumbotron/>
+      <Backdrop/>
     </Aux>
   );
 }
---
+++ src/components/Pages/Art/Jumbotron/Jumbotron.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Aux from '../../../../hoc/Aux';
+import classes from './Jumbotron.module.css';
+
+const jumbotron = () => (
+    <div className={classes.Jumbotron}>pic, name, title</div>
+)
+
+export default jumbotron;
