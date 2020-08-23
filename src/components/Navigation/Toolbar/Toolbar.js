5b1a5cefd6d9bb93637c0961ca5844197f524efe
Matt Teran <matthew.a.teran@gmail.com>
2020-08-23 01:36:16 -0500

    Added some styling to the toolbar

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import Aux from '../../../hoc/Aux';
+import classes from './Toolbar.module.css';
 
 const toolbar = (props) => (
-    <Aux>
-        <NavigationItems link='Art Portfolio'/>
-        <NavigationItems link='Web Dev Portfolio'/>
-        <NavigationItems link='Blog'/>
-        <NavigationItems link='Contact'/>
-    </Aux>
+    <div className={classes.Toolbar}>
+        <div className={classes.Logo}>
+            Hey!
+        </div>
+        <div className={classes.NavLinks}>
+            <NavigationItems link='Art Portfolio'/>
+            <NavigationItems link='Web Dev Portfolio'/>
+            <NavigationItems link='Blog'/>
+            <NavigationItems link='Contact'/>
+        </div>
+    </div>
 )
 
 export default toolbar;
