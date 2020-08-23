bec1d1751c33887800aaceec81178dd7dfe99c28
Matt Teran <matthew.a.teran@gmail.com>
2020-08-23 02:58:38 -0500

    added some images and gallery component

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import Aux from './hoc/Aux';
 import Backdrop from './components/UI/Backdrop/Backdrop';
 import Toolbar from './components/Navigation/Toolbar/Toolbar';
 import Jumbotron from './components/Pages/Art/Jumbotron/Jumbotron';
+import Gallery from './components/Pages/Art/Gallery/Gallery';
 
 function App() {
   return (
     <Aux>
       <Toolbar/>
       <Jumbotron/>
-      <Backdrop/>
+      <Gallery/>
+      {/* <Backdrop/> */}
     </Aux>
   );
 }
---
+++ src/components/Pages/Art/Gallery/Gallery.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Aux from '../../../../hoc/Aux';
+import classes from './Gallery.module.css';
+
+const gallery = (props) => (
+    <div className={classes.Gallery}>
+        <p className={classes.Title}>Art Gallery</p>
+    </div>
+)
+
+export default gallery;
